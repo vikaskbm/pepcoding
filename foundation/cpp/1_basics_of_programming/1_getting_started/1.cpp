4f7a7b97fc0cec119e6e874c695ed88e3e5d9832
Vikas Bishnoi <vikaskbm@gmail.com>
2021-02-01 15:17:49 +0530

    pepcoding -> foundation -> basics -> getting_started -> question_1 - CPP

---
+++ foundation/cpp/1_basics_of_programming/1_getting_started/1.cpp
@@ -0,0 +1,21 @@
+// Print Z
+
+
+#include<iostream>
+using namespace std;
+
+
+void print_z() {
+    cout << "*****" << endl;
+    cout << "   * " << endl;
+    cout << "  *" << endl;
+    cout << " *" << endl;
+    cout << "*****" << endl;
+}
+
+
+int main(int argc, char** agrc){
+    //write your code here
+    print_z();    
+    return 0;
+}
