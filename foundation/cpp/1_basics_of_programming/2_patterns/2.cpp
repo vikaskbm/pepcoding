4ea85df12b75a9fa153da2eeeea1395e6d79f750
Vikas Bishnoi <vikaskbm@gmail.com>
2021-02-02 16:59:24 +0530

    pepcoding -> foundation -> basics -> patterns -> question_2 - CPP

--- foundation/cpp/1_basics_of_programming/2_patterns/2.cpp
+++ foundation/cpp/1_basics_of_programming/2_patterns/2.cpp
@@ -1,23 +1,24 @@
-// Pattern 1
+// Pattern 2
 
 // 1. You are given a number n.
 // 2. You've to create a pattern of * and separated by tab as shown in output format.
 
-// n=5
+// n=5 
 
-// *	
-// *	*	
-// *	*	*	
+// *	*	*	*	*	
 // *	*	*	*	
-// *	*	*	*	*
+// *	*	*	
+// *	*	
+// *
 
 
 #include <iostream>
 using namespace std;
 
-void pattern_1(int num) {
+
+void pattern_2(int num) {
     for(int i = 1; i <= num; i++) {
-        for(int j = 1; j <= i; j++) cout << "*	";
+        for(int j = 1; j <= num+1-i; j++) cout << "*	";
         cout << endl;
     }
 }
@@ -25,5 +26,5 @@ void pattern_1(int num) {
 int main() {
     int n;
     cin >> n;
-    pattern_1(n);
+    pattern_2(n);
 }
