cd53c0624961743d11a2d2d3a9578815fd4d68d0
Vikas Bishnoi <vikaskbm@gmail.com>
2021-02-03 23:23:21 +0530

    pepcoding -> foundation -> basics -> patterns -> question_12 - CPP

--- foundation/cpp/1_basics_of_programming/2_patterns/12.cpp
+++ foundation/cpp/1_basics_of_programming/2_patterns/12.cpp
@@ -1,26 +1,29 @@
-// Pattern 11
+// Pattern 12
 
 // 1. You are given a number n.
 // 2. You've to create a pattern as shown in output format.
 
 // n=5
 
-//  1	
-//  2	3	
-//  4	5	6	
-//  7	8	9	10	
-//  11	12	13	14	15
+// 0	
+// 1	1	
+// 2	3	5	
+// 8	13	21	34	
+// 55	89	144	233	377	
 
 
 #include <iostream>
 using namespace std;
 
-void pattern_11(int num) {
-    int count = 1;
+void pattern_12(int num) {
+    int a = 0;
+    int b = 1;
     for(int i = 1; i <= num; i++) {
         for(int j = 1; j<=i; j++) {
-            cout << count << "	";
-            count++;
+            cout << a << "	";
+            int c = a+b;
+            a = b;
+            b = c;
         }
         cout << endl;
     }
@@ -29,5 +32,5 @@ void pattern_11(int num) {
 int main() {
     int n;
     cin >> n;
-    pattern_11(n);
+    pattern_12(n);
 }
